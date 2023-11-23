8fef0bf95fe09ceb959254969447412b2da2f1c7
千羽龙空 <109353356+qianyulongkong@users.noreply.github.com>
2023-11-23 20:12:36 +0800

    Update inpaint.py

--- modules/inpaint.py
+++ modules/inpaint.py
@@ -40,8 +40,8 @@ def inpaint(model_name, lora_name, lora_style, init_image_mask, prompt, negative
     if not os.path.exists("./PPdiffusers-webui/output/input_inpaint2img/mask"):
             os.makedirs("./PPdiffusers-webui/output/input_inpaint2img/mask")
     # 输入图片及图片mask保存路径
-    image_path="./PPdiffusers-webui/output/input_inpaint2img/img/tem_img_{lora_name}_{utils.out_put_num}.png"
-    mask_path="./PPdiffusers-webui/output/input_inpaint2img/mask/tem_mask_{lora_name}_{utils.out_put_num}.png"
+    image_path=f"./PPdiffusers-webui/output/input_inpaint2img/img/tem_img_{lora_name}_{utils.out_put_num}.png"
+    mask_path=f"./PPdiffusers-webui/output/input_inpaint2img/mask/tem_mask_{lora_name}_{utils.out_put_num}.png"
 
     imageio.imwrite(image_path, init_image_mask["image"])
     imageio.imwrite(mask_path, init_image_mask["mask"])
@@ -80,4 +80,4 @@ def inpaint(model_name, lora_name, lora_style, init_image_mask, prompt, negative
 
     utils.out_put_num += 1
 
-    return [Image.open(save_path)]
+    return Image.open(save_path)
